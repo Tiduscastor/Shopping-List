c469cea28344fbbaaaeefdd2b53fd6ed48838941
Patrick <83998979+Tiduscastor@users.noreply.github.com>
2021-11-21 22:07:54 -0600

    Update ShoppingList.py

--- ShoppingList.py
+++ ShoppingList.py
@@ -4,13 +4,14 @@ Created on Sun Nov 21 19:29:03 2021
 
 @author: patrick
 """
-
+# To be able to create a save file and also clear the console screen for ease of viewing output
 import os
 
+#Declaring of list and variable to name the finished file
 shopping_list = []
-
 saved_file = "shopping_list.txt"
 
+#Function to clear screen in program on new call
 def clear_console():
     # Clear the console
     if os.name == 'nt':
@@ -18,9 +19,9 @@ def clear_console():
     else:
         os.system('clear')
 
+#initial instructional screen with optional commands
 def show_help(list):
     clear_console()
-    # Print out instructions on how to use the app
     print("""
 You have {} items in your current list.
 Enter 'Help' to show this help menu again.
@@ -30,9 +31,9 @@ Enter 'Clear' to delete your current list.
 Enter 'Save' to save your new list and exit.
 Enter 'Stop' to exit without saving.""".format(len(list)))
 
+#shows current list and adds some prefacing numbers with '1.)' style
 def show_list(list):
     clear_console()
-    # Print out the list
     if len(list) == 0:
         print("You have 0 items in your list.")
     elif len(list) > 0:
@@ -42,16 +43,17 @@ def show_list(list):
             print(str(num) + ".)", item)
             num += 1
 
+#adds new item to list. Be careful with typing it as you will need the exact typing to remove item.
 def add_to_list(new_item, list):
     clear_console()
     # Add new items to the list
     list.append(new_item)
     print("Added {}. List now has {} items.".format(new_item, len(list)))
 
+#function to remove item from list
 def remove_from_list(list):
     clear_console()
     print("What item would you like to remove?\n")
-    # prompt the user for an item to remove
     word = input("Remove an item: ")
     if word in list:
         # remove item from the shopping list
@@ -62,32 +64,27 @@ def remove_from_list(list):
         clear_console()
         print("{} is not in your list.".format(word))
 
+#reads saved list information to be able to display it from the previously created file
 def open_list(filename, list):
     try:
-        # open file
         with open(filename) as file:
-            # read contents of the file
             data = file.read().splitlines()
-            # add each line in file (minus line breaks) to the current list
             list.extend(data)
-            #for line in file:
-                #list.append(line[:-1])
     except FileNotFoundError:
         pass
 
+#Save new list to file or creates one if it doesn't exist
 def save_list(filename, list):
-    # Save the current list to a text file
     with open(filename, "w") as file:
         for item in list:
             file.write(item + "\n")
-    print("\nYour list has been saved to {}. \n\nHappy shopping!\n".format(filename))
+    print("\nYour new list has been saved to {}.".format(filename))
 
+#clears the previous list
 def delete_list(filename, list):
     clear_console()
-    # clear the current list
     list.clear()
     try:
-        # delete saved list file
         os.remove(filename)
         print("\nList deleted successfully.")
     except FileNotFoundError:
@@ -101,7 +98,7 @@ while True:
     # Ask for new items
     new_item = input("\nAdd an item or enter a command: ")
 
-    # Be able to quit the app, show help, show the list, save the list, clear the list and remove an item from the list
+#reads input and acts accordingly. Input is set to Upper to offset user-input error
     if new_item.upper() == "STOP":
         show_list(shopping_list)
         print("\nHappy Shopping!\n")
